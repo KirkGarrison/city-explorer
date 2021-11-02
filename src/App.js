ec15b9be5fac8a7407c1fdaeba53a7ae74171e59
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-11-01 18:14:41 -0700

    got locationiq info in console.log

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ constructor(props) {
   }
 }
 
-handleClick = () => {
-  const url = 'https://us1.locationiq.com/v1/search.php'
+handleClick = async () => {
+  const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&q=${this.state.cityValue}&format=json`;
+  let response = await axios.get(url);
+  console.log(response.data[0]);
   
 }
 
