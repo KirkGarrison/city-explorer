2f6e9ba064574ffeac67a0251be82918b1e6200d
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-11-04 10:23:12 -0700

    added weather api

--- src/Weather.js
+++ src/Weather.js
@@ -5,22 +5,23 @@ export default class Weather extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            weatherForcast: []
+            weatherForecast: []
         }
     }
 
     getWeatherForcast = async () => {
-        const url = `${process.env.REACT_APP_SERVER_URL}/weather?city=${this.props.location.display_name.split(',')[0]}&lat=${this.props.location.lat}&lon=${this.props.location.lon}`;
-        let response = await axios.get(url)
-        console.log(response.data)
-        this.setState({ weatherForcast: response.data});
+        const url = `${process.env.REACT_APP_SERVER_URL}/weather?lat=${this.props.location.lat}&lon=${this.props.location.lon}`
+        let response = await axios.get(url);
+        // console.log(response.data);
+        // print array results on screen
+        this.setState({weatherForecast: response.data })
     }
 
     render () {
         return (
             <div>
                 <button onClick={this.getWeatherForcast}>Get Weather Forecast</button>
-                {this.state.weatherForcast.length && this.state.weatherForcast.map((dayforcast, idx) => <li key={idx}>low temp:{dayforcast.min_temp} high temp:{dayforcast.max_temp} description:{dayforcast.description}</li>)}
+                {this.state.weatherForcast.length > 0 && this.state.weatherForcast.map((dayForecast, idx) => <li key={idx}>date: {dayForecast.date} description:{dayForecast.description}</li>)}
             </div>
         )
     }
